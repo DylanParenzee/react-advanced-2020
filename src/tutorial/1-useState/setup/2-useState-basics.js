62c9d6051581289e826378770b159769c25439da
DylanParenzee <dylanparenzee@hotmail.com>
2022-07-16 12:36:56 +1000

    added useState for re-rendering titl text

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState("random title");
+
+  const handleClick = () => {
+    if (text === "random title") {
+      setText("Hello world");
+    } else {
+      setText("random title");
+    }
+  };
+  return (
+    <React.Fragment>
+      <h2>{text}</h2>
+      <button className="btn" type="button" onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
