09091e7d1b207ac7c937e42ae592c6b43e4f75ec
DylanParenzee <dylanparenzee@hotmail.com>
2022-07-21 18:30:00 +1000

    added fetch logic

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const url = 'https://api.github.com/users';
+const url = "https://api.github.com/users";
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [users, setUsers] = useState([]);
+
+  const getUsers = async () => {
+    const response = await fetch(url);
+    const users = await response.json();
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  return (
+    <>
+      <h3>Github users</h3>
+      <ul className="users">
+        {users.map((user) => {
+          return <li key={user.id}>hello</li>;
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default UseEffectFetchData;
