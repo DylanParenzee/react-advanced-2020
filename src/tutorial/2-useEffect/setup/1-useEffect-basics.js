8583885c654bc9cee825416de508aa537cf047ad
DylanParenzee <dylanparenzee@hotmail.com>
2022-07-25 08:08:03 +1000

    added conditional functionality inside of use effect hook

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -7,7 +7,9 @@ const UseEffectBasics = () => {
 
   useEffect(() => {
     console.log("call use effect");
-    document.title = `New Messages(${value})`;
+    if (value > 0) {
+      document.title = `New Messages(${value})`;
+    }
   });
 
   return (
