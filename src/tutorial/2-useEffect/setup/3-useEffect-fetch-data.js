d40926a18b69302b3e30be2f159b0a5c8e1a133d
DylanParenzee <dylanparenzee@hotmail.com>
2022-08-15 15:41:38 +1000

    finished

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -12,12 +12,26 @@ const UseEffectFetchData = () => {
         setUsers(data);
       });
   };
-
+  useEffect(() => {
+    getUsers();
+  }, []);
   return (
     <>
-      {getUsers()}
       <h3>Github users</h3>
-      <ul className="users"></ul>
+      <ul className="users">
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
