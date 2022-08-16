db6d7b808db8599aba0673aa83dae776c175f432
DylanParenzee <dylanparenzee@hotmail.com>
2022-08-16 18:43:30 +1000

    conditional rendering completed

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,43 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
+
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState("default user");
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+
+      .then((data) => {
+        const { login } = data;
+        setUser(login);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  if (isLoading)
+    return (
+      <div>
+        <h2>Loading...</h2>;
+      </div>
+    );
+  if (isError)
+    return (
+      <div>
+        <h1>Error...</h1>;
+      </div>
+    );
+
+  return (
+    <div>
+      <h1>{user}</h1>
+    </div>
+  );
 };
 
 export default MultipleReturns;
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
+
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  const toggleState = () => {
+    isError === true ? setIsError(false) : setIsError(true);
+  };
+
+  return (
+    <>
+      {isError && <h1>Error...</h1>}
+      <button className="btn" onClick={toggleState}>
+        Toggle error
+      </button>
+    </>
+  );
 };
 
 export default ShortCircuit;
