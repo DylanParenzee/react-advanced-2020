c4ffaebdffdd8cfc0145aef3c104ca9bd78ea8f8
DylanParenzee <dylanparenzee@hotmail.com>
2022-11-17 06:51:39 +1100

    typo

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -6,7 +6,55 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [people, setPeople] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (firstName && email) {
+      const person = { firstName, email };
+      setPeople((people) => {
+        return [...people, person];
+        console.log(people);
+      });
+
+      console.log(person);
+    } else {
+      console.log("empty form");
+    }
+  };
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="firstName">Name : </label>
+          <input
+            value={firstName}
+            type="text"
+            id="firstName"
+            name="firstName"
+            onChange={(e) => {
+              setFirstName(e.target.value);
+            }}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="Email">Email : </label>
+          <input
+            value={email}
+            type="text"
+            id="Email"
+            name="Email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </div>
+        <button type="submit">Add Person</button>
+      </form>
+    </>
+  );
 };
 
 export default ControlledInputs;
